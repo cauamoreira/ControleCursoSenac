090ddbb34498904c94c093c0edc8b748975b83ac
cauamoreira <moreira.caua@outlook.com>
2024-01-31 16:37:47 -0300

    criado os eventos de controles de logs

--- Controle Curso Senac/ControleAcesso.cs
+++ Controle Curso Senac/ControleAcesso.cs
@@ -15,16 +15,69 @@ namespace Controle_Curso_Senac
         public ControleAcesso()
         {
             InitializeComponent();
-           
+            CarregarHistorico();
         }
 
-      
+        public void CarregarHistorico()
+        {
+            gridHistorico.Rows.Clear();
 
-       
+            using (var bd = new BancoDeDados())
+            {
 
-       
+                var historico = bd.Historicos.ToList();
+
+                foreach (var registro in historico)
+                {
+                    gridHistorico.Rows.Add(
+                        registro.Id,
+                        registro.Login,
+                        registro.DataHora,
+                        registro.Alteracao,
+                        registro.Detalhes
+
+                    );
+                }
+            }
+        }
+
+        private void AbrirFormAgenda()
+        {
+            Agenda agenda_De_Curso = new Agenda();
+            agenda_De_Curso.Show();
+        }
+
+        private void ControleAcesso_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                DialogResult iSair = MessageBox.Show("Deseja realmente sair?",
+                                                    "Agenda de Cursos",
+                                                    MessageBoxButtons.YesNo,
+                                                    MessageBoxIcon.Question);
+
+                if (iSair == DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+                else
+                {
+
+                    AbrirFormAgenda();
+                    this.Hide();
+                }
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
 
 
 
